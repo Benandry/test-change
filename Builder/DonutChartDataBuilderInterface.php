75fc2d9b19f65ef3c4eccaf99e1ff8c25056844d
Eloi Charly <nandry556@gmail.com>
2024-02-01 15:00:20 +0100

    Remove all

--- Builder/DonutChartDataBuilderInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Builder;
-
-interface DonutChartDataBuilderInterface
-{
-  public function buildData($region): array;
-}
--- Service/MorrisBarChartDataBuilderInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Service;
-
-interface MorrisBarChartDataBuilderInterface
-{
-  // public function buildData(array $stores, array $caSkus): array;
-  public function buildData(int $storeId, string $storeName): array;
-}
